50c600b4577f94b52d7166af23f07194cd6e2e20
reingma <gabriel.aguiar@reingma.com>
2025-01-14 23:38:12 +0100

    initial setup

---
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -0,0 +1,39 @@
+-- get lazy.
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+-- put lazy into runtimepath(rtp) for neovim
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("lazy").setup({
+  spec = {
+    { "folke/tokyonight.nvim", config = function() vim.cmd.colorscheme "tokyonight" end },
+    -- add LazyVim and import its plugins
+    --{ "LazyVim/LazyVim", import = "lazyvim.plugins"},
+    -- import/override with your plugins
+    { import = "custom.plugins" },
+  },
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to 'true' to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot of the plugins that support versioning,
+    --  have outdated releases, which may break your Neovim install.
+    version = false,
+  },
+  install = { colorscheme = { "habamax" } },
+})
